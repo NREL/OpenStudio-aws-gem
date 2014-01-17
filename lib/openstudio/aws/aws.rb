f6fe0e12a2a4fb9accaf3a83b2f9748caaa7ec45
Nicholas Long <nicholas.long@nrel.gov>
2014-01-17 01:21:16 -0700

    cleanup

--- lib/openstudio/aws/aws.rb
+++ lib/openstudio/aws/aws.rb
@@ -30,26 +30,6 @@ module OpenStudio
 
         puts @os_aws.server.to_os_hash.to_json
 
-        # Call the openstudio script to start the ec2 instance 
-        #start_string = "ruby #{os_aws_file_location} #{@config.access_key} #{@config.secret_key} us-east-1 EC2 launch_server \"#{instance_string}\""
-        #puts "Server Command: #{start_string}"
-        #server_data_str = `#{start_string}`
-        #@server_data = JSON.parse(server_data_str, :symbolize_names => true)
-
-        # Save pieces of the data for passing to the worker node
-
-        #File.open(server_key_file, "w") { |f| f << @server_data[:private_key] }
-        #File.chmod(0600, server_key_file)
-
-        # Save off the server data to be loaded into the worker nodes.  The Private key needs to e read from a
-        # file in the worker node, so save that name instead in the HASH along with a couple other changes
-        #@server_data[:private_key] = server_key_file
-        #if @server_data[:server]
-        #  @server_data[:server_id] = @server_data[:server][:id]
-        #  @server_data[:server_ip] = @server_data[:server][:ip]
-        #  @server_data[:server_dns] = @server_data[:server][:dns]
-        #end
-
         File.open(server_json_filename, "w") { |f| f << JSON.pretty_generate(@os_aws.server.to_os_hash) }
 
         # Print out some debugging commands (probably work on mac/linux only)
