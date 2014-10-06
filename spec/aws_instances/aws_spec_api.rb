085674f2f2cfd043ba1795ef968a61a3ec3c83b0
Nicholas Long <nicholas.long@nrel.gov>
2014-10-06 16:39:46 -0600

    fix old api failures

--- spec/aws_instances/aws_spec_api.rb
+++ spec/aws_instances/aws_spec_api.rb
@@ -10,7 +10,7 @@ describe OpenStudio::Aws::Aws do
     end
 
     it 'should create a new instance' do
-      @aws.should_not be_nil
+      expect(@aws).to be_nil
     end
 
     it 'should create a server' do
@@ -26,7 +26,7 @@ describe OpenStudio::Aws::Aws do
       expect(File.exist?('server_data.json')).to be true
       expect(@aws.os_aws.server).not_to be_nil
 
-      h = @os_aws.server.to_os_hash
+      h = @aws.os_aws.server.to_os_hash
       expect(h[:group_id]).to be_a String
       expect(h[:group_id]).to match /^[\d\S]{32}$/
       expect(h[:location]).to eq 'AWS'
@@ -39,7 +39,7 @@ describe OpenStudio::Aws::Aws do
 
       @aws.create_workers(1, options)
 
-      expect(@aws.os_aws.workers).to have(1).thing
+      expect(@aws.os_aws.workers.size).to eq 1
       expect(@aws.os_aws.workers[0].data[:dns]).not_to be_nil
     end
 
@@ -50,7 +50,7 @@ describe OpenStudio::Aws::Aws do
       @config = OpenStudio::Aws::Config.new
       @aws_2 = OpenStudio::Aws::Aws.new
 
-      @aws_2.os_aws.find_server(@aws.os_aws.group_uuid)
+      @aws_2.os_aws.find_server(@aws.os_aws.server.to_os_hash)
 
       expect(@aws.os_aws.server).not_to be_nil
     end
@@ -80,26 +80,27 @@ describe OpenStudio::Aws::Aws do
       j = JSON.parse(File.read('server_data.json'), symbolize_names: true)
 
       config = OpenStudio::Aws::Config.new
-      aws = OpenStudio::Aws::Aws.new
+      aws2 = OpenStudio::Aws::Aws.new
 
-      aws.load_instance_info_from_file('server_data.json')
+      aws2.load_instance_info_from_file('server_data.json')
 
-      expect(aws.os_aws.server.group_uuid).to eq j[:group_id]
-      puts aws.os_aws.server.inspect
+      expect(aws2.os_aws.server.group_uuid).to eq j[:group_id]
+      puts aws2.os_aws.server.inspect
 
       local_file = File.expand_path('spec/resources/upload_me.sh')
       remote_file = '/home/ubuntu/i_uploaded_this_file.sh'
 
-      aws.upload_file(:server, local_file, remote_file)
 
-      aws.shell_command(:server, "source #{remote_file}")
+      aws2.upload_file(:server, local_file, remote_file)
+
+      aws2.shell_command(:server, "source #{remote_file}")
 
       FileUtils.rm_f 'success.receipt' if File.exist? 'success.receipt'
-      aws.download_remote_file(:server, '/home/ubuntu/success.receipt', 'success.receipt')
+      aws2.download_remote_file(:server, '/home/ubuntu/success.receipt', 'success.receipt')
       expect(File.exist?('success.receipt')).to be true
 
       FileUtils.rm_f 'gemlist.receipt' if File.exist? 'gemlist.receipt'
-      aws.download_remote_file(:server, '/home/ubuntu/gemlist.receipt', 'gemlist.receipt')
+      aws2.download_remote_file(:server, '/home/ubuntu/gemlist.receipt', 'gemlist.receipt')
       expect(File.exist?('gemlist.receipt')).to be true
       gemlist = File.read('gemlist.receipt')
       expect(gemlist).to match /^s3 \(.*\)$/
