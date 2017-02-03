761ef8a4b116d8f1edf3e3790044600ec4d3eecc
Ry Horsey <horsey.henry@gmail.com>
2017-02-02 23:43:23 -0700

    Is this really a single vs double quotes issue???

--- lib/openstudio/lib/openstudio_aws_wrapper.rb
+++ lib/openstudio/lib/openstudio_aws_wrapper.rb
@@ -482,7 +482,7 @@ class OpenStudioAwsWrapper
     logger.info('Running the configuration script for the worker(s).')
     @workers.each { |worker| worker.wait_command('sudo /home/ubuntu/worker_provision.sh && echo "true"') }
     logger.info('Successfully re-sized storage devices for all nodes. Joining server nodes to the swarm.')
-    @workers.each { |worker| worker.wait_command("#{File.read(swarm_file)} && echo 'true'") }
+    @workers.each { |worker| worker.wait_command("#{File.read(swarm_file)} && echo \"true\"") }
     logger.info('All worker nodes have been added to the swarm. Starting the server cluster.')
     @server.wait_command('docker stack deploy --compose-file docker-compose.yml osserver-stack')
     @logger.info('The OpenStudio Server stack has been started. Scaling worker nodes.')
